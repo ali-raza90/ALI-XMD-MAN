288c94890e08d94851a41536dcb52ed92bd70547
𝐀𝐋𝐈ャ <ar6167423@gmail.com>
2025-01-24 00:37:08 +0500

    Add files via upload

---
+++ set.js
@@ -0,0 +1,52 @@
+const fs = require('fs-extra');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('set.env'))
+    require('dotenv').config({ path: __dirname + '/set.env' });
+const path = require("path");
+const databasePath = path.join(__dirname, './database.db');
+const DATABASE_URL = process.env.DATABASE_URL === undefined
+    ? databasePath
+    : process.env.DATABASE_URL;
+module.exports = { session: process.env.SESSION_ID || '',
+    PREFIXE: process.env.PREFIX || ".",
+    OWNER_NAME: process.env.OWNER_NAME || "Fredi Ezra",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 9230035xxx",              
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "no",
+    AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
+    BOT : process.env.BOT_NAME || 'LUCKY_MD',
+    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg',
+    MODE: process.env.PUBLIC_MODE || "no",
+    PM_PERMIT: process.env.PM_PERMIT || 'no',
+    HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
+    HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
+    WARN_COUNT : process.env.WARN_COUNT || '5' ,
+    ETAT : process.env.PRESENCE || '',
+    CHATBOT : process.env.PM_CHATBOT || 'no',
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
+    ANTICALL : process.env.ANTICALL || 'no',
+                  AUTO_REACT : process.env.AUTO_REACT || 'no',
+                  AUDIO_REPLY : process.env.AUDIO_REPLY|| 'yes', 
+                  AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'no',
+                  AUTO_REPLY : process.env.AUTO_REPLY || 'no',
+                  AUTO_READ : process.env.AUTO_READ || 'no',
+                  AUTO_SAVE_CONTACTS : process.env.AUTO_SAVE_CONTACTS || 'no',
+                  AUTO_REJECT_CALL : process.env.AUTO_REJECT_CALL || 'no',
+                  AUTO_BIO : process.env.AUTO_BIO || 'yes',
+                  AUDIO_REPLY : process.env.AUDIO_REPLY || 'no',
+                  AUTO_TAG_STATUS : process.env.AUTO_TAG_STATUS || 'no',
+    DATABASE_URL,
+    DATABASE: DATABASE_URL === databasePath
+        ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
+   
+};
+let fichier = require.resolve(__filename);
+fs.watchFile(fichier, () => {
+    fs.unwatchFile(fichier);
+    console.log(`mise à jour ${__filename}`);
+    delete require.cache[fichier];
+    require(fichier);
+});
+
+
+                  
